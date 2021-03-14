b58e5ccf7ffee0a5683dcac2562551ec267820e1
Khirru <78700526+Khirru@users.noreply.github.com>
2021-03-14 22:31:06 +0100

    Create Basic2.tsx

---
+++ Basic2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Basic2" tilewidth="32" tileheight="32" spacing="1" tilecount="64" columns="7">
+ <image source="floortileset.png" width="256" height="256"/>
+</tileset>
